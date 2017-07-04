c5e30468bcae5730a4cf8ad52d4acb30977df458
KimCoding <jeokrang@gmail.com>
2017-07-04 14:43:04 +0900

    Write example code for Chapter Three

--- scraptest.py
+++ scraptest.py
@@ -1,24 +1,55 @@
+
 from urllib.request import urlopen
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 import re
+import urllib
+import datetime
+import random
+import ssl
 
-html = urlopen("http://www.pythonscraping.com/pages/page3.html")
-bsObj = BeautifulSoup(html, "html.parser")
+pages = set()
+gcontext = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+random.seed(datetime.datetime.now())
 
-print("### siblings")
-for sibling in bsObj.find("table", {"id": "giftList"}).tr.next_siblings:
-    print(sibling)
+def getInternalLinks(bsObj, includeUrl):
+    includeUrl = urlparse(includeUrl).scheme + "://" + urlparse(includeUrl).netloc
+    internalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(/|.*" + includeUrl + ")")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in internalLinks:
+                if link.attrs['href'].startswith("/"):
+                    internalLinks.append(includeUrl + link.attrs['href'])
+                else:
+                    internalLinks.append(link.attrs['href'])
+    return internalLinks
 
-images = bsObj.findAll("img", {"src": re.compile("\.\.\/img\/gifts/img.*\.jpg")})
+def getExternalLinks(bsObj, excludeUrl):
+    externalLinks = []
+    for link in bsObj.findAll("a", href=re.compile("^(http|www)((?!" + excludeUrl + ").)*$")):
+        if link.attrs['href'] is not None:
+            if link.attrs['href'] not in externalLinks:
+                externalLinks.append(link.attrs['href'])
+    return externalLinks
 
-print("")
-print("### images")
-for image in images:
-    print(image["src"])
+def getRandomExternalLink(startingPage):
+    html = urlopen(startingPage, context=gcontext)
+    bsObj = BeautifulSoup(html, "html.parser")
+    externalLinks = getExternalLinks(bsObj, urlparse(startingPage).netloc)
 
-print("")
-print("### tags with two attributes")
-tagsWithTwoAttrs = bsObj.findAll(lambda tag: len(tag.attrs) == 2)
-for tag in tagsWithTwoAttrs:
-    print(tag)
+    if len(externalLinks) == 0:
+        domain = urlparse(startingPage).shceme + "//" + urlparse(startingPage).netloc
+        internalLinks = getInternalLinks(bsObj, domain)
+        return getRandomExternalLink(internalLinks[random.randint(0, len(internalLinks) - 1)])
+    else:
+        return externalLinks[random.randint(0, len(externalLinks) - 1)]
+
+def followExternalOnly(startingSite):
+    externalLink = getRandomExternalLink(startingSite)
+    print("Random external link is: " + externalLink)
+    followExternalOnly(externalLink)
+
+followExternalOnly("http://oreilly.com")
+
+
 
